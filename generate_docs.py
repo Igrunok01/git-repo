007bf226af446d1282b993fd3918758f84e97bb9
Даниил <igrunok02@gmail.com>
2025-11-30 01:24:32 +0300

    Remove old docs generator and pyc

--- generate_docs.py
+++
@@ -1,9 +0,0 @@
-from datetime import datetime
-import os
-
-os.makedirs("docs", exist_ok=True)
-
-with open("docs/info.txt", "w", encoding="utf-8") as f:
-    f.write("Документация к лабораторной работе по CI/CD\n")
-    f.write("Проект: простая функция безопасного деления и тесты.\n")
-    f.write(f"Последнее обновление: {datetime.utcnow().isoformat()} UTC\n")
